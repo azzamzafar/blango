1425b468214e2c8234231be4bf946fb9c531d234
Syed Zafar <zafarazzam.syed123@gmail.com>
2022-08-24 16:00:10 +0000

    Finish custom filters

--- blango/urls.py
+++ blango/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import blog.views
+from django.urls import path,include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/",blog.views.index)
+    path("blog/",include('blog.urls')),
 ]
