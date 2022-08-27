ba842cb2d01a9abc128bc73d0cb87ee6b44afbd2
Syed Zafar <zafarazzam.syed123@gmail.com>
2022-08-27 17:45:00 +0000

    Finish security n passwords

--- blango/settings.py
+++ blango/settings.py
@@ -163,7 +163,13 @@ class Dev(Configuration):
             'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
         },
     ]
-
+    # Password hashing & salting algorithms
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
