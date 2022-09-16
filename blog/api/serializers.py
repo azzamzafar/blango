37a404800d32748099167fddb65d0f013eb591f8
Syed Zafar <zafarazzam.syed123@gmail.com>
2022-09-16 18:07:59 +0000

    Finish Serializers

---
+++ blog/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from blog.models import Post
+from django.utils.text import slugify
+
+
+class PostSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(required=False)
+    autogenerate_slug = serializers.BooleanField(required=False
+    ,write_only=True, default=False)
+    class Meta:
+        model = Post
+        fields = "__all__"
+        readonly = ["modified_at", "created_at"]
+    
+    def validate(self, data):
+      if not data.get("slug"):
+        if data["autogenerate_slug"]:
+          data["slug"] = slugify(data["title"])
+        else:
+          raise serializers.ValidationError("slug is required if autogenerate_slug is not set")
+        del data["autogenerate_slug"]
+        return data
+
+    
--- blog/api_views.py
+++ blog/api_views.py
@@ -1,40 +1,43 @@
 import json
 from http import HTTPStatus
-
 from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from blog.models import Post
+from blog.api.serializers import PostSerializer
 
-def post_to_dict(post):
-  return {
-    "pk":post.pk,
-    "author_id": post.author_id,
-    "created_at": post.created_at,
-    "modified_at": post.modified_at,
-    "published_at": post.published_at,
-    "title": post.title,
-    "slug": post.slug,
-    "summary": post.summary,
-    "content": post.content,
-  }
+# def post_to_dict(post):
+#   return {
+#     "pk":post.pk,
+#     "author_id": post.author_id,
+#     "created_at": post.created_at,
+#     "modified_at": post.modified_at,
+#     "published_at": post.published_at,
+#     "title": post.title,
+#     "slug": post.slug,
+#     "summary": post.summary,
+#     "content": post.content,
+#   }
 
 @csrf_exempt
 def post_list(request):
   if request.method == "GET":
     posts = Post.objects.all()
-    posts_as_dict = [post_to_dict(p) for p in posts]
-    return JsonResponse({"data": posts_as_dict})
+    return JsonResponse({"data": PostSerializer(posts,many=True).data})
 
   elif request.method == "POST":
     post_data = json.loads(request.body)
-    post = Post.objects.create(** post_data)
+    post_data["autogenerate_slug"]=True
+    serializer = PostSerializer(data=post_data)
+    serializer.is_valid(raise_exception=True)
+    post = serializer.save()
+
     return HttpResponse(
       status = HTTPStatus.CREATED,
       headers={
-        "Location": reverse("api_post_detail", args=(posts.pk,))},
+        "Location": reverse("api_post_detail", args=(post.pk,))},
     )
   return HttpResponseNotAllowed(["GET", "POST"])
 
@@ -43,14 +46,16 @@ def post_detail(request, pk):
   post = get_object_or_404(Post, pk=pk)
   
   if request.method=="GET":
-    return JsonResponse(post_to_dict(post))
+    return JsonResponse(PostSerializer(post).data)
   
   elif request.method=="PUT":
-    post_data = json.loads(requst.body)
-    for field, value in post_data.items():
-      setattr(post, field, value)
-    post.save()
+    post_data = json.loads(request.body)
+    post_data["autogenerate_slug"]=True
+    serializer = PostSerializer(post,data=post_data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
     return HttpResponse(status=HTTPStatus.NO_CONTENT)
+  
   elif request.method=="DELETE":
     post.delete()
     return HttpResponse(status=HTTPStatus.NO_CONTENT)
